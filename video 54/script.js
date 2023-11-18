594d5aa857833376e56534b223b94ef4e7826d26
Alph702 <147134651+Alph702@users.noreply.github.com>
2023-11-18 21:07:36 +0500

    Video 54

---
+++ video 54/script.js
@@ -0,0 +1,14 @@
+alert("Hello world!")
+
+console.log("Code is runing...")
+console.log("Code is also runing...")
+
+let a = prompt("Enter the number")
+let isTrue = confirm('Are you want to your computer runing or not')
+
+if (isTrue) {
+    console.log("OK run your computer. Thank you for coming")
+}
+else {
+console.log([12, 3,4 ,5,6 ,5, 34] + "Your computer is crashed...??SD{AS??ASDkPASASUOUEU*(QWKLAIPASD{DPPADP{{}}ASD{}A}{}DOASD}AWAPDIWPQP{")
+}
